edad1e9682686f53f790e884bc17496fa6b85f88
rosaj <rosaj@unipu.hr>
2016-11-15 14:17:39 +0100

    d

--- Osobni_Troškovnik/DatumChooseWindow.cs
+++ Osobni_Troškovnik/DatumChooseWindow.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+using Gtk;
+namespace Osobni_Troškovnik
+{
+	public partial class DatumChooseWindow : Gtk.Window
+	{
+
+		public delegate void eventHandler(DateTime odDatum, DateTime doDatum);
+		public event eventHandler signaliziraj;
+
+		public DatumChooseWindow() : base(Gtk.WindowType.Toplevel)
+		{
+			this.Build();
+			this.Icon = this.RenderIcon("Icon", IconSize.Menu, null);
+			this.Title = "Odaberi raspon";
+		}
+
+		protected void filtrirajClicked(object sender, EventArgs e)
+		{
+			signaliziraj(kalendarOd.GetDate(), kalendarDo.GetDate());
+			Destroy();
+		}
+
+		protected void onDeleteEvent(object o, Gtk.DeleteEventArgs args)
+		{
+			signaliziraj(DateTime.Parse("1/1/2000"), DateTime.Now);
+			Destroy();
+		}
+	}
+}
