03f49ed66d012bceeda949787adbd7f5f46d6a98
rosaj <rosaj@unipu.hr>
2016-11-15 14:20:01 +0100

    d

--- Osobni_Troškovnik/NovaKategorijaWidow.cs
+++ Osobni_Troškovnik/NovaKategorijaWidow.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+using Gtk;
+namespace Osobni_Troškovnik
+{
+
+
+	public partial class NovaKategorijaWidow : Gtk.Window
+	{
+		public delegate void eventHandler(string ime);
+		public event eventHandler resurs;
+		public NovaKategorijaWidow() : base(Gtk.WindowType.Toplevel)
+		{
+			this.Build();
+			this.Icon = this.RenderIcon("Icon", IconSize.Menu, null);
+			this.Title = "Dodaj novu kategoriju";
+		}
+
+		public void spremiKategorijuClicked(object sender, EventArgs e)
+		{
+			resurs(novaKategorija.Text);
+			this.Destroy();
+
+		}
+
+
+	}
+}
