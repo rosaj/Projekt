5a225a219a2fc45273f78136ea7073e9298ff1dc
rosaj <rosaj@unipu.hr>
2016-11-15 14:22:25 +0100

    d

--- Osobni_Troškovnik/Trosak.cs
+++ Osobni_Troškovnik/Trosak.cs
@@ -1,1 +1,58 @@
-﻿
+﻿using System;
+namespace Osobni_Troškovnik
+{
+	public class Trosak
+	{
+		private string kategorija;
+		private float cijena;
+		private string datum;
+		private string opis;
+
+
+		public Trosak(string kategorija, float cijena, string datum, string opis)
+		{
+			this.kategorija = kategorija;
+			this.cijena = cijena;
+			this.datum = datum;
+			this.opis = opis;
+		}
+
+
+
+		public string Kategorija
+		{
+			get
+			{
+				return kategorija;
+			}
+
+		}
+
+		public float Cijena
+		{
+			get
+			{
+				return cijena;
+			}
+
+		}
+
+		public string Datum
+		{
+			get
+			{
+				return datum;
+			}
+
+		}
+
+		public string Opis
+		{
+			get
+			{
+				return opis;
+			}
+
+		}
+	}
+}
